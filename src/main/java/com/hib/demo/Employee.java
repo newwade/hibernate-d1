218badf42d7e893ccb6b651762930848b82dba01
abhilash <abhilashwithh@gmail.com>
2021-09-07 17:42:20 +0530

    initial commit

---
+++ src/main/java/com/hib/demo/Employee.java
@@ -0,0 +1,53 @@
+package com.hib.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+public class Employee {
+		@Id
+	   private int id;
+	   private String firstName; 
+	   private String lastName;   
+	   private int salary;  
+
+	   public Employee() {}
+	   public Employee(String fname, String lname, int salary) {
+	      this.firstName = fname;
+	      this.lastName = lname;
+	      this.salary = salary;
+	   }
+	   
+	   public int getId() {
+	      return id;
+	   }
+	   
+	   public void setId( int id ) {
+	      this.id = id;
+	   }
+	   
+	   public String getFirstName() {
+	      return firstName;
+	   }
+	   
+	   public void setFirstName( String first_name ) {
+	      this.firstName = first_name;
+	   }
+	   
+	   public String getLastName() {
+	      return lastName;
+	   }
+	   
+	   public void setLastName( String last_name ) {
+	      this.lastName = last_name;
+	   }
+	   
+	   public int getSalary() {
+	      return salary;
+	   }
+	   
+	   public void setSalary( int salary ) {
+	      this.salary = salary;
+	   }
+	}
